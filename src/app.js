e06f0359053fd5dc739b4c8c08e84aea627a5a61
Andreas McDermott <andreasmcdermott@gmail.com>
2015-12-21 03:57:45 +0000

    Started on adapting view to play field.

--- src/app.js
+++ src/app.js
@@ -6,15 +6,14 @@ playground({
   canvasHeight: 480,
   
   preload: function () {
-    this.scale = this.width / this.canvasWidth;
   },  
   create: function () {
-
-  },  
-  ready: function () {
+    this.scale = this.width / this.canvasWidth;
     this.min = {x: 0, y: 0};
     this.max = {x: 0, y: 0};
     this.entities = [];
+  },  
+  ready: function () {
     this.entities.push(new Entity(0, 0));
     this.entities.push(new Entity(-200, -200));
     this.entities.push(new Entity(0, -150));
@@ -41,6 +40,11 @@ playground({
   },
   render: function () {
     this.layer.clear('black');
+    
+    var w = this.max.x - this.min.x;
+    var h = this.max.y - this.min.y;
+    var padding = 20;
+    
     for (var i = 0; i < this.entities.length; ++i) {
       var entity = this.entities[i];
       this.layer.fillStyle('red');
